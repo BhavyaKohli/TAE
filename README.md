e233b3843e34b212f9dd54290ad25fd04b15f592
Bhavya Kohli <bhavyakohli2024@gmail.com>
2023-06-12 10:38:40 +0530

    updated readme following repository refactor

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Optimizing TAE training and improving ease of tensorizing different models
 
-* The `warmup_test.ipynb` notebook in the `module/` directory contains the basic code for the model and training, with sanity checks along the way to confirm operation.
-* `train_utils.py` contains a few utility functions for creating progress bars, defining datasets, and a simple autoencoder training function. `train_utils_features.py` is a notebook illustrating the use of the utils file.
+* `feature_test.ipynb` contains the basic code for the model and training, with sanity checks along the way to confirm operation.
+* `code/train_utils.py` contains a few utility functions for creating progress bars, defining datasets, and training functions for simple AEs and the TAE. `train_utils_feature_demo.ipynb` illustrates the use of some of the features.
+* `code/models.py` contains the definition of a base autoencoder class which can be used to define both linear and convolutional AEs.
+* `code/tae.py` contains the definition of the TensorizedAutoencoder.
 * `base.py` contains a few utility functions, not specific to the project
 * 
--- code/models.py
+++ code/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Block(nn.Module):
     def __init__(self, layer_type: nn.Module, in_features, out_features, activation=None, **layer_kwargs):
         super().__init__()
@@ -10,11 +11,13 @@ class Block(nn.Module):
     def forward(self, x):
         if self.act is None:
             return self.layer(x)
-        
+
         return self.act(self.layer(x))
 
+
 class BaseAutoencoder(nn.Module):
-    def __init__(self, layer_type: nn.Module, enc_channels, dec_channels, bias=True, activations=nn.ReLU(), device=None, **layer_kwargs):
+    def __init__(self, layer_type: nn.Module, enc_channels, dec_channels, bias=True, activations=nn.ReLU(), device=None,
+                 **layer_kwargs):
         """
         REQUIRES: device, enc_channels, dec_channels, activations (nn.ReLU(), nn.GELU(), etc)
         """
@@ -25,17 +28,22 @@ class BaseAutoencoder(nn.Module):
         self.activations = activations
         self.device = device or 'cpu'
 
-        if self.enc_channels[-1] != self.dec_channels[0]: 
-            print("[WARN] First shape of dec_channels does not match the terminal channel in enc_channels, proceeding with additional layer...")
-            self.dec_channels = (self.enc_channels[-1],)+self.dec_channels
+        if self.enc_channels[-1] != self.dec_channels[0]:
+            print("[WARN] First shape of dec_channels does not match the terminal channel in enc_channels, proceeding "
+                  "with additional layer...")
+            self.dec_channels = (self.enc_channels[-1],) + self.dec_channels
 
         self.enc = nn.Sequential()
-        for i in range(len(self.enc_channels)-1):
-            self.enc.add_module(f'enc_dense{i}', Block(layer_type, self.enc_channels[i], self.enc_channels[i+1], bias=self.bias, activation=self.activations, **layer_kwargs).to(self.device))
+        for i in range(len(self.enc_channels) - 1):
+            self.enc.add_module(f'enc_dense{i}',
+                                Block(layer_type, self.enc_channels[i], self.enc_channels[i + 1], bias=self.bias,
+                                      activation=self.activations, **layer_kwargs).to(self.device))
 
         self.dec = nn.Sequential()
-        for i in range(len(self.dec_channels)-1):
-            self.dec.add_module(f'dec_dense{i}', Block(layer_type, self.dec_channels[i], self.dec_channels[i+1], bias=self.bias, activation=self.activations, **layer_kwargs).to(self.device))
+        for i in range(len(self.dec_channels) - 1):
+            self.dec.add_module(f'dec_dense{i}',
+                                Block(layer_type, self.dec_channels[i], self.dec_channels[i + 1], bias=self.bias,
+                                      activation=self.activations, **layer_kwargs).to(self.device))
 
     def forward(self, x, return_embed=False):
         x = x.to(self.device).float()
@@ -44,6 +52,7 @@ class BaseAutoencoder(nn.Module):
 
         if return_embed: return embed, out
         return out
+
 
 class GroupedModel(nn.Module):
     def __init__(self, n_clusters, model_class, **kwargs):
@@ -72,14 +81,14 @@ class GroupedModel(nn.Module):
         """
         x, centers = x.to(self.device), centers.to(self.device)
         embed, out = self.AE[0](x - centers[0].reshape(x.shape[1:]), True)
-        embed, out = embed.reshape((len(x),1)+embed.shape[1:]), out.reshape((len(x),1)+out.shape[1:])
+        embed, out = embed.reshape((len(x), 1) + embed.shape[1:]), out.reshape((len(x), 1) + out.shape[1:])
 
         for n in range(1, self.n_clusters):
             e, o = self.AE[n](x - centers[n].reshape(x.shape[1:]), True)
-            embed = torch.cat((embed, e.reshape((len(x),1)+e.shape[1:])), dim=1)
-            out = torch.cat((out, o.reshape((len(x),1)+o.shape[1:])), dim=1)
+            embed = torch.cat((embed, e.reshape((len(x), 1) + e.shape[1:])), dim=1)
+            out = torch.cat((out, o.reshape((len(x), 1) + o.shape[1:])), dim=1)
 
         return self._return_embed(embed, out, return_embed)
-    
+
     def forward(self, *args):
         raise NotImplementedError("Use one of `forward_with_centers` or `forward_with_clust`")
